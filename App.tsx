3cda6c73610f15f0332bdeb691a9c22bd17d4d0e
haithamassoli <haithamassoli44@gmail.com>
2022-04-13 15:40:18 +0300

    adding libraries

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "red" },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: "green" },
+        tabBarActiveTintColor: "blue",
+        // headerShown: false,
+      }}
+    >
+      <BottomTabs.Screen name="AllExpenses" component={Home} />
+      <BottomTabs.Screen name="asd" component={Home} />
+      <BottomTabs.Screen name="AllEasdasdxpenses" component={Home} />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ title: "hielfsdf" }}>
+          <Stack.Screen name="HI" component={HomeScreen} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
